613aeb09598c1ff8b40a217e5729bf4301e338b4
Josh Mason <121649751+josh-w-mason@users.noreply.github.com>
2023-03-31 12:29:05 +1300

    mainboard component calling Thunk

--- client/reducers/index.ts
+++ client/reducers/index.ts
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux'
+import theReducer from './bulletinsReducer'
 
 // import stuff from './stuff'
 
 export default combineReducers({
+  
+  currentBulletinState: theReducer
   // stuff
 })
